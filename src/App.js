c78899ff3b1b914470f05150fae5fd7c29686b46
Akash Satpute <akashsatpute150@gmail.com>
2023-03-14 18:14:42 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Navbar from './navbar';
+import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './NotFound';
+function App() 
+{
+  
   return (
+    <Router>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="content">
+       <Routes>
+        <Route  exact path="/" Component={Home}/>
+        <Route exact path="/create" Component={Create}/>
+        <Route exact path="/blogs/:id" Component={BlogDetails}/>
+        <Route exact path="*" Component={NotFound}>
+         
+        </Route>
+       </Routes>
+       
+      </div>
+      
     </div>
+    </Router>
   );
 }
+
 
 export default App;
